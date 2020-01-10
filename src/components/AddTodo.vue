421eb76407ad81d26a6bf48456ee9721ec46e018
Eranda Methmal <emkarachchi@gmail.com>
2020-01-11 01:37:45 +0530

    Minor Improvements

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <form @submit="addTodo">
+            <input v-model="title" type="text" name="title" placeholder="Add Todo...">
+            <input class="btn" type="submit" value="Submit" >
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"AddTodo",
+    data(){
+        return{
+            title:'',
+        }
+    },
+    methods:{
+        addTodo(e){
+            e.preventDefault();
+            const newTodo = {
+                title:this.title,
+                completed:false,
+            }
+            //this should be sent to parent
+            this.$emit('add-todo',newTodo);
+            this.title = '';
+        }
+    }
+}
+</script>
+<style scoped>
+    form{
+        display: flex;
+    }
+    input[type='text']{
+          flex: 10;
+          padding: 10px;  
+    }
+    input[type=submit]{
+        flex: 2;
+    }
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete" />
+      {{todo.title}}
+      <button @click="$emit('del-todo',todo.id)" class="del">x</button>
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+};
+</script>
+<style scoped>
+.todo-item {
+  padding: 10px;
+  border-bottom: 1px #000 dotted;
+  text-align: left;
+}
+.is-complete {
+  text-decoration: line-through;
+}
+.del{
+    background-color: red;
+}
+</style>
